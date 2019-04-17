d0ad29b3f5613c4fd431a7c2a32caefa913560b3
anon-gitter <anongitter@gmail.com>
2019-04-17 12:02:17 +0900

    use pthread for libvpx crash

---
+++ userlog.h
@@ -0,0 +1,11 @@
+﻿#ifndef _USERLOG_H_
+#define _USERLOG_H_
+
+#define LOG_TRACE(...) printf( __VA_ARGS__ )
+#define LOG_DEBUG(...) printf( __VA_ARGS__ )
+#define LOG_INFO(...) printf( __VA_ARGS__ )
+#define LOG_WARN(...) printf( __VA_ARGS__ )
+#define LOG_ERROR(...) printf( __VA_ARGS__ )
+#define LOG_CRITICAL(...) printf( __VA_ARGS__ )
+
+#endif
---
+++ vpxConverter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+int bmp2vpx(unsigned char* bmpData, int width, int height, 
+	unsigned char** vp9RetBuf, long *vp9BufSize);
+int vpx2bmp(unsigned char *vp9buf, int vp9BufSize, 
+	unsigned char** bmpBuf, int *retWidth, int *retHeight);
+int fileWriteReadTest(void);
+#ifdef __cplusplus
+}
+#endif
